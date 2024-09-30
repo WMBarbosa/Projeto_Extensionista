39c14a4f90f3234b20ed8a52492a97f35ed5278a
WMBarbosa <wesleymario01@gmail.com>
2024-09-30 10:00:57 -0300

    Criação do projeto de extensão

---
+++ src/main/java/com/mariowesley/projeto_extensionista/ProjetoExtensionistaApplication.java
@@ -0,0 +1,13 @@
+package com.mariowesley.projeto_extensionista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoExtensionistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoExtensionistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mariowesley/projeto_extensionista/ProjetoExtensionistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariowesley.projeto_extensionista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoExtensionistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
