19f07ca035ddedf735f06d7a1c20730cf45434da
WMBarbosa <wesleymario01@gmail.com>
2024-10-03 09:00:14 -0300

    Update class enum Category and Role

--- src/main/java/com/mariowesley/projeto_extensionista/entities/enums/Category.java
+++ src/main/java/com/mariowesley/projeto_extensionista/entities/enums/Category.java
@@ -1,4 +1,13 @@
 package com.mariowesley.projeto_extensionista.entities.enums;
 
 public enum Category {
+    ALIMENTACAO,
+    TECNOLOGIA,
+    MODA,
+    EDUCACAO,
+    SAUDE,
+    SERVICOS,
+    ENTRETENIMENTO
+
+
 }
